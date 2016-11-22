803ba07490594021d7942c47cad705aa9f1c5092
Rattanak Chea <rattanak.chea@gmail.com>
2016-11-21 22:34:37 -0800

    resolve submodule issue

---
+++ prototype/app.js
@@ -0,0 +1,11 @@
+// could use an movie db api
+
+var data = [
+    {
+        title: 'Ice Age: Collision Course',
+        image: '//images.redbox.com/Images/EPC/boxartlarge/8421.jpg',
+        description: 'Scrat’s epic pursuit of the elusive acorn catapults him into the universe where he accidentally sets off a series of cosmic events that transform and threaten the Ice Age World. To save themselves, Sid, Manny, Diego, and the rest of the herd must leave their home and embark on a quest full of comedy and adventure, traveling to exotic new lands and encountering a host of colorful new characters.'
+    }
+
+
+]
